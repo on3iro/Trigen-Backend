cecc8fe32e2650dc2d17a2965dbd0f3b21bb07b4
PrettyPinkPonyOfDeath <martin.hauke.jr@gmail.com>
2017-03-30 11:44:51 +0200

    Using flask-restful for simple api;

--- app/triserv.py
+++ app/triserv.py
@@ -1,15 +1,27 @@
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import reqparse, abort, Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
+USERS = {
+    'user1': {'unique hash': '3h4ncj9'},
+    'user2': {'unique hash': 's8doh3l'},
+    'user3': {'unique hash': 'M8mHq1l'},
+}
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
 
-api.add_resource(HelloWorld, '/')
+def abort_if_user_doesnt_exist(user_id):
+    if user_id not in USERS:
+        abort(404, message="User {} does not exist".format(user_id))
+
+
+class User(Resource):
+    def get(self, user_id):
+        abort_if_user_doesnt_exist(user_id)
+        return USERS[user_id]
+
+api.add_resource(User, '/users/<user_id>')
 
 # TODO: disable in production
 if __name__ == '__main__':
