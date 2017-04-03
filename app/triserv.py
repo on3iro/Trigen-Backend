24879b115e6335c4ede72017948ce6c2466f63d8
sox <sox@simplel.de>
2017-04-03 21:11:56 +0200

    modified host and port to be accessible from the internet

--- app/triserv.py
+++ app/triserv.py
@@ -199,4 +199,4 @@ def custom_auth_response_callback(access_token, identity):
 
 # TODO: disable in production
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
